3741fd7302668ebf07efb76d1de6476424c196e2
filip <f.szumotalski@gmail.com>
2024-03-06 22:18:33 +0100

    Fully connected to the api

--- Weather App/Views/WelcomeView.swift
+++ Weather App/Views/WelcomeView.swift
@@ -2,43 +2,7 @@
 //  WelcomeView.swift
 //  Weather App
 //
-//  Created by Filip Szumotalski on 19/02/2024.
+//  Created by Filip Szumotalski on 03/03/2024.
 //
 
-import SwiftUI
-import CoreLocationUI
-
-struct WelcomeView: View {
-    @EnvironmentObject var locationManager: LocationManager
-
-    var body: some View {
-        VStack {
-            VStack(spacing: 20) {
-                Text("Welcome to the Weather App")
-                    .bold()
-                    .font(.title)
-                
-                Text("Please share your current location to get the weather in your area")
-                    .padding()
-            }
-            .multilineTextAlignment(.center)
-            .padding()
-
-            
-            // LocationButton from CoreLocationUI framework imported above, allows us to requestionLocation
-            LocationButton(.shareCurrentLocation) {
-                locationManager.requestLocation()
-            }
-            .cornerRadius(30)
-            .symbolVariant(.fill)
-            .foregroundColor(.white)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity).background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.blue.opacity(0.2)]), startPoint: .top, endPoint: .bottom))
-    }
-}
-
-struct WelcomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        WelcomeView()
-    }
-}
+import Foundation
